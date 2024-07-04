4618a5b6c27bdbf48e7a055ac428db3ac24e67aa
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2024-07-04 12:44:25 +0900

    updateBounds

--- include/ik_constraint2_body_contact/BodyContactConstraint.h
+++ include/ik_constraint2_body_contact/BodyContactConstraint.h
@@ -6,7 +6,23 @@
 namespace ik_constraint2_body_contact{
   class BodyContactConstraint : public ik_constraint2::PositionConstraint {
   public:
-  protected:
+    // A_linkの接触候補点の中から選んだA_localposとB_link中のB_localposを一致させる
+    // 接触点の法線方向は考慮されるが、接平面の方向の自由度は制約しない. 法線方向の回転自由度の重みを0にすること.
+    // contact_pos_link: 接触候補点探索用. variablesとして追加すること. このtranslationやrotationはA_linkのローカル座標系
+    // contactPoints: 接触点候補
+    const cnoid::LinkPtr& contact_pos_link() const { return contact_pos_link_;}
+    cnoid::LinkPtr& contact_pos_link() { return contact_pos_link_;}
+    const std::vector<cnoid::Isometry3>& contactPoints() const { return contactPoints_;}
+    std::vector<cnoid::Isometry3>& contactPoints() { return contactPoints_;}
+    // 探索された接触点位置をもとに、PositionConstraintのA_localpos_を更新する
+    virtual void updateBounds() override;
+    // 複製する. このとき、modelMapのkeyにあるロボットモデルに属するリンクは、valueに置き換える
+    virtual std::shared_ptr<ik_constraint2::IKConstraint> clone(const std::map<cnoid::BodyPtr, cnoid::BodyPtr>& modelMap) const override;
+    void copy(std::shared_ptr<BodyContactConstraint> ret, const std::map<cnoid::BodyPtr, cnoid::BodyPtr>& modelMap) const;
+
+  private:
+    cnoid::LinkPtr contact_pos_link_ = nullptr;
+    std::vector<cnoid::Isometry3> contactPoints_;
   };
 }
 
--- src/BodyContactConstraint.cpp
+++ src/BodyContactConstraint.cpp
@@ -1,5 +1,33 @@
 #include <ik_constraint2_body_contact/BodyContactConstraint.h>
 
-namespace ik_constraint_body_contact{
-  
+namespace ik_constraint2_body_contact{
+  void BodyContactConstraint::updateBounds() {
+    // 探索された接触点位置をもとに、PositionConstraintのA_localpos_を更新する
+    // 最も近い接触点候補を選ぶ
+    double maxValue = -1e3;
+    double weight = 0.1;
+    cnoid::Isometry3 selectedContact;
+    for (int i=0; i<this->contactPoints_.size(); i++) {
+      double value=weight*(this->contactPoints_[i].linear()*cnoid::Vector3::UnitZ()).dot(this->contact_pos_link_->R() * cnoid::Vector3::UnitZ());
+      value -= (this->contactPoints_[i].translation() - this->contact_pos_link_->p()).norm();
+      if (value > maxValue) {
+        maxValue = value;
+        selectedContact = this->contactPoints_[i];
+      }
+    }
+    this->A_localpos() = selectedContact;
+    PositionConstraint::updateBounds();
+  }
+  std::shared_ptr<ik_constraint2::IKConstraint> BodyContactConstraint::clone(const std::map<cnoid::BodyPtr, cnoid::BodyPtr>& modelMap) const {
+    std::shared_ptr<BodyContactConstraint> ret = std::make_shared<BodyContactConstraint>(*this);
+    this->copy(ret, modelMap);
+    return ret;
+  }
+
+  void BodyContactConstraint::copy(std::shared_ptr<BodyContactConstraint> ret, const std::map<cnoid::BodyPtr, cnoid::BodyPtr>& modelMap) const {
+    if(this->A_link() && modelMap.find(this->A_link()->body()) != modelMap.end()) ret->A_link() = modelMap.find(this->A_link()->body())->second->link(this->A_link()->index());
+    if(this->B_link() && modelMap.find(this->B_link()->body()) != modelMap.end()) ret->B_link() = modelMap.find(this->B_link()->body())->second->link(this->B_link()->index());
+    if(this->eval_link() && modelMap.find(this->eval_link()->body()) != modelMap.end()) ret->eval_link() = modelMap.find(this->eval_link()->body())->second->link(this->eval_link()->index());
+  }
+
 }
