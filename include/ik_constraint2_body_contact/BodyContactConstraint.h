10298c4e309241540cfb6fc8304e8f099475cfbd
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2024-07-04 10:46:36 +0900

    initial commit

---
+++ include/ik_constraint2_body_contact/BodyContactConstraint.h
@@ -0,0 +1,13 @@
+#ifndef IK_CONSTRAINT2_BODYCONTACTCONSTRAINT_H
+#define IK_CONSTRAINT2_BODYCONTACTCONSTRAINT_H
+
+#include <ik_constraint2/ik_constraint2.h>
+
+namespace ik_constraint2_body_contact{
+  class BodyContactConstraint : public ik_constraint2::PositionConstraint {
+  public:
+  protected:
+  };
+}
+
+#endif
---
+++ src/BodyContactConstraint.cpp
@@ -0,0 +1,5 @@
+#include <ik_constraint2_body_contact/BodyContactConstraint.h>
+
+namespace ik_constraint_body_contact{
+  
+}
